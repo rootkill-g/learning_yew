d2c6f8ee24309491f858945eb962547c5ec7a28f
etch1000 <110674620+etch1000@users.noreply.github.com>
2023-03-05 00:20:22 +0530

    Conditional rendering, lists

--- hello_yew/dist/hello_yew-a5b92d8339109fc9.js
+++ hello_yew/dist/hello_yew-a5b92d8339109fc9.js
@@ -354,6 +354,11 @@ function getImports() {
         wasm.__wbindgen_free(arg0, arg1 * 4);
         console.error(...v0);
     };
+    imports.wbg.__wbg_log_1f7f93998ab961f7 = function(arg0, arg1) {
+        var v0 = getArrayJsValueFromWasm0(arg0, arg1).slice();
+        wasm.__wbindgen_free(arg0, arg1 * 4);
+        console.log(...v0);
+    };
     imports.wbg.__wbg_instanceof_Window_e266f02eee43b570 = function(arg0) {
         let result;
         try {
@@ -580,12 +585,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper3493 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 216, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper4056 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 231, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3622 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 233, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper4185 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 248, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -623,7 +628,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('hello_yew-a6339045dc81d00e_bg.wasm', import.meta.url);
+        input = new URL('hello_yew-a5b92d8339109fc9_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
--- hello_yew/src/lib.rs
+++ hello_yew/src/lib.rs
@@ -1,11 +1,51 @@
+use gloo::console::log;
+use serde::{Deserialize, Serialize};
 use yew::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct LangName {
+    username: String,
+    fav_lang: String,
+}
 
 #[function_component]
 pub fn App() -> Html {
+    let name = "etch1000";
+    let lang_name = LangName {
+        username: name.to_owned(),
+        fav_lang: String::from("Rust Programming Language"),
+    };
+
+    log!("My name is :", name);
+    log!(serde_json::to_string_pretty(&lang_name).ok());
+
+    let class = "header title";
+    let class_p = "paragraph";
+
+    let msg: Option<&str> = None;
+
+    let num_list = (1..=10).collect::<Vec<_>>();
+
     html! {
         <>
-        <h1>{"Hello Yew"}</h1>
-        <p>{"We are going Full Stack Now"}</p>
+        <h1 class={class}>{"Hello Yew"}</h1>
+        if class_p == "paragraph" {
+            <p>{"We are going Full Stack Now"}</p>
+        }
+
+        if let Some(message) = msg {
+            <p>{message}</p>
+        } else {
+            <p>{"No messages to show"}</p>
+        }
+
+        <ul class="item-list">
+            { to_li(num_list) }
+        </ul>
         </>
     }
+}
+
+fn to_li<T: std::fmt::Display>(list: Vec<T>) -> Vec<Html> {
+    list.iter().map(|item| html! {<li>{item}</li>}).collect()
 }
