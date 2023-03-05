0d063d8bbf58dd57208555c63ea5fd32852096ef
etch1000 <110674620+etch1000@users.noreply.github.com>
2023-03-05 08:42:24 +0530

    Handling multiple states

--- hello_yew/src/components/molecules/custom_form.rs
+++ hello_yew/src/components/molecules/custom_form.rs
@@ -5,17 +5,25 @@ use yew::prelude::*;
 #[function_component(CustomForm)]
 pub fn custom_form() -> Html {
     let username_state = use_state(|| "default".to_owned());
+    let button_click_count_state = use_state(|| 0u32);
     let cloned_username_state = username_state.clone();
     let username_changed = Callback::from(move |username: String| {
         cloned_username_state.set(username.clone());
         log!("Username was changed to :", username);
     });
+    let cloned_button_click_count_state = button_click_count_state.clone();
+    let button_clicked = Callback::from(move |_| {
+        let counter = *cloned_button_click_count_state;
+        cloned_button_click_count_state.set(counter + 1u32);
+        log!("Button was clicked");
+    });
 
     html! {
-        <form>
+        <div>
             <TextInput name="username" handle_onchange={username_changed} />
-            <CustomButton label="Submit" />
+            <CustomButton label="Submit" onclick={button_clicked} />
             <p>{"Username : "}{&*username_state}</p>
-        </form>
+            <p>{"Button click counter : "}{*button_click_count_state}</p>
+        </div>
     }
 }
